83367937b018b46c574858cebb8281107288f7db
Nikhil Chavan <chavannikhil1007@gmail.com>
2023-01-12 19:54:30 +0530

    second commit

--- src/Components/Simulation.js
+++ src/Components/Simulation.js
@@ -7,11 +7,6 @@ const Simulation = () => {
   
     <iframe width="725" height="453" src="https://www.tinkercad.com/embed/hzFMfQH9PqU?editbtn=1" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
 
-    <iframe width="725" height="453" src="https://www.tinkercad.com/embed/akyvitbFfCr?editbtn=1" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
-
-    <iframe width="725" height="453" src="https://www.tinkercad.com/embed/gPOfjv9Srsv?editbtn=1" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
-
-    <iframe width="725" height="453" src="https://www.tinkercad.com/embed/gj5tDXXBGm9?editbtn=1" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
 
     </div>
     </div>
